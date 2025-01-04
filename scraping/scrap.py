c8f541101ac9b36a7098949c5cdeb3d8217a34d3
jnrslm3 <jumabekovn43@gmail.com>
2025-01-04 15:43:50 +0600

    second commit

--- scraping/scrap.py
+++ scraping/scrap.py
@@ -8,7 +8,22 @@ def get_links():
         links.append(f'{link}{i}')
     return links
 
+def get_book_links():
+    links = get_links()
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36'}
 
+    books_links = []
+
+    for url in links: 
+        response = requests.get(url, headers=headers)
+        soup = BS(response.text, 'html.parser')
+        blocks = soup.find_all('div', class_="e4xwgl-1 gEQwGK")
+        for b in blocks:
+            a = b.find('a').get('href')
+            books_links.append(f"https://mybook.ru{a}")
+        
+    return books_links
 
 
 
